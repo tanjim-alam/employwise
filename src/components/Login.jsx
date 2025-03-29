7b60b0b9d400b07a35ad9574e39698407a519194
Tanjim Alam <tanjimseo@gmail.com>
2025-03-29 10:40:39 +0530

    updated login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
           </div>
           <button
             type="submit"
-            className="w-full px-4 py-2 text-white bg-blue-600 rounded-md hover:bg-blue-700 transition"
+            className="w-full px-4 py-2 text-white cursor-pointer bg-blue-600 rounded-md hover:bg-blue-700 transition"
           >
             Login
           </button>
